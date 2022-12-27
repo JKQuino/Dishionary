77aee68a6cc01b136409def25dec3a89b0d242c5
Erica Delambaca <72697082+dlmbc@users.noreply.github.com>
2022-12-27 14:04:54 +0800

    Recipe and Pantry Update

--- Dishionary/www/js/pantry.js
+++ Dishionary/www/js/pantry.js
@@ -7,6 +7,54 @@ let noRecipeCards = document.getElementById('noRecipe');
 
 let tags = [];
 
+window.onload = function() {
+	// Retrieve the ingredients list and the search results from the session storage
+	let ingredients = JSON.parse(sessionStorage.getItem('ingredients'));
+	let pantryResults = JSON.parse(sessionStorage.getItem('pantryResults'));
+  
+	// Check if the ingredients and pantryResults variables are not null
+	if (ingredients && pantryResults) {
+		noRecipeCards.style.display = "none";
+		recipeContainer.classList.remove('position-relative');
+		
+		// Clear the recipeCardContainer element
+		if (recipeCardContainer.childNodes.length > 0) { recipeCardContainer.innerHTML=""; }
+  
+	  	// Loop through the ingredients list and display them on the page
+	  	for (var i = 0; i < ingredients.length; i++) {
+			let ingredient = document.createElement('div');
+			ingredient.innerHTML = `
+		 		<span onclick="removeIngredient(this, '${ingredients[i]}')" class="p-2 badge border rounded-pill bg-light text-dark fs-6 fw-normal">
+					<span>${ingredients[i]}</span>
+					<i class="ms-2 fas fa-times text-danger"></i>
+		  		</span>
+		`;
+		ingredientContainter.appendChild(ingredient);
+	  }
+  
+	// Loop through the search results and display them on the page
+	for (var i = 0; i < pantryResults.length; i++) {
+		let recipeCard = document.createElement('div');
+		recipeCard.setAttribute('class', 'recipe-card card mb-3');
+		recipeCard.innerHTML = `
+		  	<div class="row g-0">
+				<div class="col-5 col-sm-5">
+				<img id="recipeImage" src="${pantryResults[i].image}" class="img-fluid rounded-start h-100">
+				</div>
+				<div class="col-7 col-sm-7">
+					<div class="card-body">
+						<h5 class="card-title text-truncate">${pantryResults[i].title}</h5>
+						<p class="card-text">Rating: # <i class="fas fa-star"></i><br> Favorite By: # <i class="fas fa-users"></i></p>
+						<a href="./recipe.html?id=${pantryResults[i].id}" class="card-text"><small class="text-muted">Tap to check recipe</small></a>
+					</div>
+				</div>
+		  	</div>
+		`;
+		recipeCardContainer.appendChild(recipeCard);
+	  }
+	}
+} 
+
 function generateResult(){
 	noRecipeCards.style.display = "none";
 	recipeContainer.classList.remove('position-relative');
@@ -31,6 +79,10 @@ function generateResult(){
 	};
 	
 	$.ajax(settings).done(function (response) {
+		// Store the search results in the session storage
+		sessionStorage.setItem('ingredients', JSON.stringify(tags));
+		sessionStorage.setItem('pantryResults', JSON.stringify(response));
+		
 		for (var i = 0; i < response.length; i++) {
 			let recipeCard = document.createElement('div');
 			recipeCard.setAttribute('class', 'recipe-card card mb-3');
