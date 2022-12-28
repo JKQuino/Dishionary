0b79d11031e6239a42dca31731857fc4d01a296e
John Kenneth B. Quiño <110990403+JKQuino@users.noreply.github.com>
2022-12-29 00:22:06 +0800

    Index/Search Files v2
    modify home.js and index.js
    (remove comment in index.js to work)

---
+++ Dishionary/www/js/home.js
@@ -0,0 +1,29 @@
+var options = {
+  wrapAround: true,
+  accessibility: true,
+  pageDots: true,
+  setGallerySize: false,
+  prevNextButtons: false,
+  autoPlay: 4000,
+  arrowShape: {
+    x0: 10,
+    x1: 60,
+    y1: 50,
+    x2: 60,
+    y2: 45,
+    x3: 15
+  }
+};
+
+//Sets the data variable tow data-carousel
+var carousel = document.querySelector('[data-carousel]');
+var slides = document.getElementsByClassName('carousel-cell');
+var flkty = new Flickity(carousel, options);
+
+flkty.on('scroll', function () {
+  flkty.slides.forEach(function (slide, i) {
+    var image = slides[i];
+    var x = (slide.target + flkty.x) * -1/3;
+    image.style.backgroundPosition = x + 'px';
+  });
+});
